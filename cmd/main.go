50b2f8004b4236f00dca1cbd71f8bacb66c7ac3c
Ckala62rus <agr.akyla@mail.ru>
2022-05-25 21:00:00 +0300

    add service, repository, clear architecture

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,16 @@ import (
 
 	"github.com/Ckala62rus/rest-go"
 	"github.com/Ckala62rus/rest-go/pkg/handler"
+	"github.com/Ckala62rus/rest-go/pkg/repository"
+	"github.com/Ckala62rus/rest-go/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	// handlers := new(handler.Handler)
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(rest.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
