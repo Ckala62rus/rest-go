e178dfa9594f610d01cf275bff613b8bd3b58488
Ckala62rus <agr.akyla@mail.ru>
2022-05-25 20:26:14 +0300

    add handler and routes

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	"github.com/Ckala62rus/rest-go"
+	"github.com/Ckala62rus/rest-go/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(rest.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal("error occured while running http server: %s", err.Error())
 	}
 }
