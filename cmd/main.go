b613fad0a96d0d8bbd408c08cec1e5527ce0b281
Ckala62rus <agr.akyla@mail.ru>
2022-05-25 21:11:49 +0300

    add configure package viper and init config in main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -7,17 +7,26 @@ import (
 	"github.com/Ckala62rus/rest-go/pkg/handler"
 	"github.com/Ckala62rus/rest-go/pkg/repository"
 	"github.com/Ckala62rus/rest-go/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	// handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(rest.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
